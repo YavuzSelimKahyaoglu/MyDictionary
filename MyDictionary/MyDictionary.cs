d3b398c6db01b30c9ed618af63ede4cb16eca15e
Yavuz Selim Kahyaoğlu <68874461+YavuzSelimKahyaoglu@users.noreply.github.com>
2021-01-21 18:20:29 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+
+        T[] value;
+        T[] key;
+        public MyDictionary()
+        {
+            value=new T[0];
+            key = new T[0];
+        }
+
+        public void Add(T value1,T key1)
+        {
+            T[] tempValue = value;
+            T[] tempKey= key;
+
+            value = new T[tempValue.Length+1];
+            key = new T[tempKey.Length+1];
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                value[i] = tempValue[i];
+                key[i] = tempKey[i];
+            }
+
+
+            value[value.Length - 1]=value1;
+            key[key.Length - 1] = key1;
+
+        }
+       
+        public int Count()
+        {
+            return value.Length;
+        }
+
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+
+            
+
+            myDictionary.Add("armut","pear");
+            myDictionary.Add("elma", "apple");
+
+            Console.WriteLine(myDictionary.Count());
+
+
+        }
+    }
+}
